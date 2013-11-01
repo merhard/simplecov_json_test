6e3173b276d547d15bdb3867af7e24f8fa64ca6d
Matt Erhard <matthew.erhard@egov.com>
2013-11-01 11:56:27 -0400

    Add simplecov support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+if RUBY_VERSION >= '1.9'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
+
 require 'rubygems'
 require 'simplecov_json_test'
+# require 'json'
 
 require 'bundler'
 Bundler.setup
